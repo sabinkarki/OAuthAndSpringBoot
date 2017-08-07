9838a9f27694e1ba42abc0b648e707400a700e92
sabin <nibaserc@gmail.com>
2017-08-07 14:10:59 -0500

    Oauth Client added as child

---
+++ com-client-oauth/src/main/java/com/client/oauth/ComClientOauthApplication.java
@@ -0,0 +1,12 @@
+package com.client.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComClientOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComClientOauthApplication.class, args);
+	}
+}
---
+++ com-client-oauth/src/test/java/com/client/oauth/ComClientOauthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.client.oauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComClientOauthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
