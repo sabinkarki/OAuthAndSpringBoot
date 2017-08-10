1e4b39830a5aba6c6e60850a7f4573590e41dd79
sabin <nibaserc@gmail.com>
2017-08-10 17:08:05 -0400

    Enums added for client

--- com-client-oauth/src/main/java/com/client/oauth/domain/enums/Gender.java
+++ com-client-oauth/src/main/java/com/client/oauth/domain/enums/Gender.java
@@ -4,5 +4,5 @@ package com.client.oauth.domain.enums;
  * Created by sabin on 8/7/2017.
  */
 public enum Gender {
-    male, female
+    MALE  , FEMALE
 }
--- com-client-oauth/src/main/java/com/client/oauth/domain/enums/Status.java
+++ com-client-oauth/src/main/java/com/client/oauth/domain/enums/Status.java
@@ -4,5 +4,5 @@ package com.client.oauth.domain.enums;
  * Created by sabin on 8/7/2017.
  */
 public enum Status {
-    active, inactive
+    ACTIVE, INACTIVE
 }
