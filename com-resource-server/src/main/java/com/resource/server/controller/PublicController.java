bfea0dbb3318a2dd75880b8ca0b4c454c369e188
sabin <nibaserc@gmail.com>
2017-08-10 17:09:42 -0400

    PublicController and ResourceController added

--- com-resource-server/src/main/java/com/resource/server/controller/PublicController.java
+++ com-resource-server/src/main/java/com/resource/server/controller/PublicController.java
@@ -1,7 +1,19 @@
 package com.resource.server.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by sabin on 8/10/2017.
  */
+@RestController
+@RequestMapping(value = "/public")
 public class PublicController {
+
+    @GetMapping(value = "/resource")
+    public String resource() {
+        return "Here is public resource. This is to show that when a resource "
+                + "is configured for public access, it doesn't need to go through the OAuth flow.";
+    }
 }
--- com-resource-server/src/main/java/com/resource/server/controller/ResourceController.java
+++ com-resource-server/src/main/java/com/resource/server/controller/ResourceController.java
@@ -1,7 +1,25 @@
 package com.resource.server.controller;
 
+import com.resource.server.domain.Patient;
+import com.resource.server.service.PatientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 /**
  * Created by sabin on 8/10/2017.
  */
+@RestController
 public class ResourceController {
+
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping(value = "/resource/patient/all")
+    public List<Patient> findAll() {
+        return patientService.findAll();
+    }
 }
