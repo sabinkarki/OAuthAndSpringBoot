800097e54bf62aae31a73f4a32d40aa91a4a6ed0
sabin <nibaserc@gmail.com>
2017-08-13 21:59:35 -0400

    Authorize server refactored

--- com-auth-server/src/main/java/com/auth/server/config/OAuth2Config.java
+++ com-auth-server/src/main/java/com/auth/server/config/OAuth2Config.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
  */
 @Configuration
 @EnableAuthorizationServer
+@EnableResourceServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 
     @Autowired
